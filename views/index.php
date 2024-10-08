776ca70a367af955f9aed5fd2d69c6ee847e38d3
venzo <vita09tech05mute@gmail.com>
2024-10-08 14:24:24 +0200

    Creata navbar e importato tailwind tramite cdn

---
+++ views/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Fifa Youth Growth Prediction</title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        <!-- Link a un font da Google Fonts (opzionale) -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <?php
+            include 'components/navbar.php';
+        ?>
+    </body>
+</html>
