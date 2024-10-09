c53f9780937ada751a28e5a5d2a0f1b483b1912b
venzo <vita09tech05mute@gmail.com>
2024-10-09 12:31:24 +0200

    finita l'interfaccia grafica con aggiunta di placeholder temporanei

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-800">
+<nav class="bg-lime-950">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -29,15 +29,15 @@
                 <div class="hidden sm:ml-6 sm:block">
                     <div class="flex space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+                        <a href="#" class="rounded-md bg-lime-700 px-3 py-2 text-sm font-medium text-white" aria-current="page">Dashboard</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-lime-600	 hover:text-white">Team</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-lime-600	 hover:text-white">Projects</a>
+                        <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-lime-600	 hover:text-white">Calendar</a>
                     </div>
                 </div>
             </div>
             <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <button type="button" class="relative rounded-full bg-lime-950	 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                     <span class="absolute -inset-1.5"></span>
                     <span class="sr-only">View notifications</span>
                     <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
--- views/index.php
+++ views/index.php
@@ -9,6 +9,8 @@
         <script src="https://cdn.tailwindcss.com"></script>
 
         <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+        <link rel="stylesheet" href="static/css/div-center.css">
     </head>
     <body>
 
@@ -17,7 +19,7 @@
             include 'components/navbar.php';
             ?>
 
-            <div class="w-full flex justify-between items-center bg-gray-200 p-4">
+            <div class="w-full flex justify-between items-center bg-lime-900 p-4">
                 <!-- Contenuto a sinistra -->
                 <div class="flex-1 text-left">
                     <p class="text-gray-700">
@@ -44,23 +46,220 @@
 
             </div>
 
-            <div class="w-full flex justify-between items-center bg-pink-200 p-4 h-72 ">
+            <div class="w-full flex justify-between items-center bg-lime-800 p-4 h-72 ">
                 <!-- Contenuto a sinistra -->
                 <div class="flex-1 text-left">
-                    <p class="text-gray-700"></p>
+                    <p class="text-gray-700">
+                        <img src="static/img/cristiano-ronaldo.png" width="287px">
+                    </p>
                 </div>
 
                 <!-- Contenuto al centro -->
-                <div class="flex-1 text-center">
-                    <p class="text-gray-700"></p>
+                <div class="player-card bg-lime-900 text-white p-4 rounded-lg shadow-lg w-2/3 h-auto mx-auto">
+                    <div>
+                        <h1 class="text-2xl font-bold">Cristiano Ronaldo dos Santos Aveiro</h1>
+                        <div class="flex items-center space-x-2">
+                            <!-- Bandiera -->
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg" alt="Portugal Flag" class="w-6 h-6">
+                            <!-- Posizioni -->
+                            <span class="bg-green-700 text-xs font-semibold px-2 py-1 rounded">LW</span>
+                            <span class="bg-blue-700 text-xs font-semibold px-2 py-1 rounded">ST</span>
+                        </div>
+                        <p class="text-sm mt-2">32y.o. (Feb 5, 1985) 185cm / 6'1" 80kg / 176lbs</p>
+                    </div>
+
+                    <!-- Statistiche giocatore -->
+                    <div class="flex justify-between mt-4">
+                        <div class="stat">
+                            <div class="text-green-500 text-lg font-bold">94</div>
+                            <div class="text-gray-400 text-xs">Overall rating</div>
+                        </div>
+                        <div class="stat">
+                            <div class="text-green-500 text-lg font-bold">94</div>
+                            <div class="text-gray-400 text-xs">Potential</div>
+                        </div>
+                    </div>
                 </div>
 
-                <!-- Contenuto a destra -->
-                <div class="flex-1 text-right">
-                    <p class="text-gray-700"></p>
-                </div>
 
             </div>
+
+            <div class="border-t border-gray-300 mx-6"></div>
+
+            <div>
+                <div class="flex player-attributes bg-lime-900 text-white p-6 rounded-lg shadow-lg w-auto mt-0">
+                    <div class="flex-1 grid grid-cols-3 gap-6">
+                        <!-- Attacking -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Attacking</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>Crossing</span> <span class="bg-green-700 px-2 py-1 rounded">85</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Finishing</span> <span class="bg-green-700 px-2 py-1 rounded">94</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Heading accuracy</span> <span class="bg-green-700 px-2 py-1 rounded">88</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Short passing</span> <span class="bg-green-700 px-2 py-1 rounded">83</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Volleys</span> <span class="bg-green-700 px-2 py-1 rounded">88</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Skill -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Skill</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>Dribbling</span> <span class="bg-green-700 px-2 py-1 rounded">91</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Curve</span> <span class="bg-green-700 px-2 py-1 rounded">81</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>FK Accuracy</span> <span class="bg-green-500 px-2 py-1 rounded">76</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Long passing</span> <span class="bg-green-500 px-2 py-1 rounded">77</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Ball control</span> <span class="bg-green-700 px-2 py-1 rounded">93</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Movement -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Movement</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>Acceleration</span> <span class="bg-green-700 px-2 py-1 rounded">89</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Sprint speed</span> <span class="bg-green-700 px-2 py-1 rounded">91</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Agility</span> <span class="bg-green-700 px-2 py-1 rounded">89</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Reactions</span> <span class="bg-green-700 px-2 py-1 rounded">96</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Balance</span> <span class="bg-yellow-500 px-2 py-1 rounded">63</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Power -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Power</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>Shot power</span> <span class="bg-green-700 px-2 py-1 rounded">94</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Jumping</span> <span class="bg-green-700 px-2 py-1 rounded">95</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Stamina</span> <span class="bg-green-700 px-2 py-1 rounded">92</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Strength</span> <span class="bg-green-500 px-2 py-1 rounded">80</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Long shots</span> <span class="bg-green-700 px-2 py-1 rounded">92</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Mentality -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Mentality</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>Aggression</span> <span class="bg-yellow-500 px-2 py-1 rounded">63</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Interceptions</span> <span class="bg-red-500 px-2 py-1 rounded">29</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Att. Position</span> <span class="bg-green-700 px-2 py-1 rounded">95</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Vision</span> <span class="bg-green-700 px-2 py-1 rounded">85</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Penalties</span> <span class="bg-green-700 px-2 py-1 rounded">85</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Composure</span> <span class="bg-green-700 px-2 py-1 rounded">95</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Defending -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Defending</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>Marking</span> <span class="bg-red-500 px-2 py-1 rounded">22</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Standing tackle</span> <span class="bg-red-500 px-2 py-1 rounded">31</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>Sliding tackle</span> <span class="bg-red-500 px-2 py-1 rounded">23</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Goalkeeping -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Goalkeeping</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">
+                                    <span>GK Diving</span> <span class="bg-red-500 px-2 py-1 rounded">7</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>GK Handling</span> <span class="bg-red-500 px-2 py-1 rounded">11</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>GK Kicking</span> <span class="bg-red-500 px-2 py-1 rounded">15</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>GK Positioning</span> <span class="bg-red-500 px-2 py-1 rounded">14</span>
+                                </li>
+                                <li class="flex justify-between mb-2">
+                                    <span>GK Reflexes</span> <span class="bg-red-500 px-2 py-1 rounded">11</span>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Traits -->
+                        <div>
+                            <h3 class="text-lg font-bold mb-2">Traits</h3>
+                            <ul>
+                                <li class="flex justify-between mb-2">Power free-kick</li>
+                                <li class="flex justify-between mb-2">Flair</li>
+                                <li class="flex justify-between mb-2">Long shot taker (AI)</li>
+                                <li class="flex justify-between mb-2">Speed dribbler (AI)</li>
+                                <li class="flex justify-between mb-2">Through ball</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="ml-6">
+                        <img src="static/img/graphic-player.png" alt="Grafico" class="w-64 h-auto rounded-lg shadow-md" />
+                    </div>
+                </div>
+            </div>
+
+
         </div>
 
         <script src="static/js/vue.js"></script>
